ee297b9ee0d40e27b526d4fa487618168628d715
maxence468 <maxencebarth12@gmail.com>
2025-12-10 10:38:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 const back = document.getElementById("myVideo");
-let i = -800;
-
-/*
-document.documentElement.addEventListener('mousewheel', function(e){
-	i += e.deltaY;
-  pre.style.left = i + 'px';
-})
-*/
-
 back.style.transition = "all 2s"
 
-function zoom(event){
-	i += event.deltaY;
-	//back.style.left = -i + 'px';
+function scroll(event){
 	back.style.left = 0;	
-	
-	
 }
 
-document.documentElement.addEventListener("wheel", zoom, { passive: false });
+document.documentElement.addEventListener("wheel", scroll, { passive: false });
