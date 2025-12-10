87a61f505ace3e3e59fc11af32792aee684ced04
maxence468 <maxencebarth12@gmail.com>
2025-12-10 22:44:08 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const back = document.getElementById("back");
-back.style.transition = "all 2s"
+const btn = document.getElementById("btn");
+back.style.transition = "all 2s";
+
+btn.style.display="none";
 
 function scroll(event){
-	back.style.left = 0;	
+	back.style.left = '20vh';
+	btn.style.display="flex";
+
 }
 
 document.documentElement.addEventListener("wheel", scroll, { passive: false });
